e1c536842ffd332008b66a8554812f72c7e56844
libby <libbyseminar@outlook.com>
2023-08-24 01:15:42 +0300

    chat working

--- chatApp.py
+++ chatApp.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, request,redirect
-import csv,os
 
+from flask import Flask, render_template, redirect, url_for, request,redirect ,session
+import csv
+from datetime import datetime
 app = Flask(__name__)
 room_names = []
-
+app.secret_key = "ABC"
 
 @app.route('/register',methods=['GET','POST'])
 def homePage():
@@ -34,8 +35,9 @@ def login():
                  if user[1] != userpass:
                      return "wrong password, try again"
                  else:
+                      session['username'] = username
                       return redirect("/lobby")
-             return "no such username, try again or register"
+         return "no such username, try again or register"
     return render_template('login.html')
     
 
@@ -43,12 +45,36 @@ def login():
 def lobbyPage():
     if request.method == 'POST':
         new_room = request.form['new_room']
-        if new_room not in os.listdir("./rooms"):
+        if new_room not in room_names:
            room_names.append(new_room)
            #room_file = open('./rooms/${room}.txt', 'w') 
            room_file = open(f'./rooms/{new_room}.txt', 'w')  
            room_file.close()
     return render_template('lobby.html',room_names=room_names)
+
+@app.route('/chat/<id>', methods=['GET', 'POST'])
+def chat(id):
+    return render_template('chat.html', room=id)
+
+
+@app.route('/api/chat/<id>', methods=['GET', 'POST'])
+def update_chat(id):
+    if request.method == "POST":
+        message = request.form['msg']
+        username = session.get('username')  # Use get to avoid errors if username is not set
+    
+        if not username:
+            return "You are not logged in.", 403  # Return a forbidden status code
+    
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+        with open(f'rooms/{id}.txt', 'a') as file:
+            file.write(f'[{timestamp}] {username}: {message}\n')
+    
+    with open(f'rooms/{id}.txt', 'r') as file:
+        all_data = file.read()
+    
+    return all_data
 
 
 if __name__ == '__main__':
