50687e3878ecd10174ecb7949cecaf0bddf624f2
yehuditHoro <yehuditush@outlook.com>
2023-08-23 09:37:52 +0300

    chat project

---
+++ src/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def homePage():
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port='5000', debug='True')
